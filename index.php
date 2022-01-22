eb9a0afa576c91e03486a7b1a96470a74edf04dd
aleksssc <alexcardinha14@gmail.com>
2022-01-22 00:11:51 +0000

    titulo realterado

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $user = new UserClasses();
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <link rel="shortcut icon" href="src/img/bankchange_logo.png" />
-    <title> | Main Pagina</title>
+    <title> | Main Page</title>
 </head>
 
 <body>
